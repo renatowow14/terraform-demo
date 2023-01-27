82c6b0b481491577970cf915877828dcbe588409
renatowow14 <renatogomessilverio@gmail.com>
2023-01-27 15:16:01 -0300

    Changes 02

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# terraform-demo
+# Terraform-demo
+
+## docs: https://mrbits.com.br/melhores-praticas-com-terraform/
+## docs: https://medium.com/@hmalgewatta/setting-up-an-aws-ec2-instance-with-ssh-access-using-terraform-c336c812322f
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
   ami             = var.instance_ami
   instance_type   = var.instance_type
   key_name        = var.instance_key_name
-  security_groups = [var.instance_security_group_name]
+  security_groups = ["${aws_security_group.ingress-all-test.id}"]
   tags = {
     Name = var.instance_name
   }
